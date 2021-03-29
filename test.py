a3abff48c647ee58ff3f4f1abc5686573d967c61
Muneeb Ul Islam <mui850@usask.ca>
2021-03-29 13:32:39 -0600

    Tackled the errors by creating 404.html and 500.html

--- test.py
+++ test.py
@@ -1,13 +1,29 @@
 """
 A module that tests the databse for the users. It can check the funcionality of creating, updating and deleting
-the User infomation in the databse. 
+the User  and saved Recipe infomation in the databse. 
 """
-from app.models import User
+from app.models import User, Recipe_db
+from app import db
 def test():
-    users = {"john":"john@hotmail.com", "kevin": "kevin@hotmail.com", "marry": "marry@gmail.com"}
-    for user_name in users:
-        db_users = User(username = user_name, email = users[user_name])
-        print(db_users.username, db_users.email)
+    if User.query.all():  # if User table database is not empty
+        db_users = User.query.all()
+        for u in db_users:
+            db.session.delete(u) # delete all users
+    
+    
+
+    # dummy data for users.
+    users = {"john":"john@hotmail.com", "kevin": "kevin@hotmail.com", "marry": "marry@gmail.com",\
+    "crazy":"crazy@gmail.com" }
+    for usr in users:
+        db_users = User(username = usr, email = users[usr])
+        db.session.add(db_users)
+    # print the users after insert
+    db_users = db.Users.query.all()
+    for u in db_users:
+        print("username: "+u.username+ "email: "+u.email)
+
+
     
 
     
